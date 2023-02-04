a95f44475ad84830000280bf97e6e41d1d2fa0da
Guudz <Kilian1702@outlook.com>
2023-02-04 02:42:45 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,30 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page('client/html/ui.html')
+
+server_scripts {  
+	'locale.lua',
+	'locales/en.lua',
+	'locales/tr.lua', 
+	'config.lua',
+	'server.lua'
+}
+
+
+client_scripts {
+	'locale.lua',
+	'locales/tr.lua',
+	'locales/en.lua', 
+	'config.lua',
+	'client/client.lua'
+}
+
+export 'openUI'
+
+files {
+	'client/html/ui.html',
+    'locale.js',
+    'client/html/metropolis.medium.otf',
+    'client/html/metropolis.bold.otf',
+    'client/html/rescale.js'
+}
